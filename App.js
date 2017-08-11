eab0996e11e0d397f7102ac9f8817116bad51d1a
Jerry Black <jerry@creativeparanoia.co.uk>
2017-08-11 21:34:27 +0100

    Added Nested Arrays + used method to fetch data

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    launch: function () {
+    mySimpleArray: ['Jerry', 'Kate', 'Rick', 'Sam'],
+    myNestedArray: [
+        { Name: 'Jerry', Age: '53', Sex: 'Male', Location: 'Plymouth' },
+        { Name: 'Kate', Age: '50', Sex: 'Female', Location: 'Portland' },
+        { Name: 'Rick', Age: '43', Sex: 'Male', Location: 'Nuneaton' },
+        { Name: 'Sam', Age: '33', Sex: 'Female', Location: 'Nuneaton' },
+    ],
+    launch: function() {
         this._myMethod();
-        ftyth
     },
-    _myMethod: function () {
 
+
+    _myMethod: function() {
+        console.log(this.myNestedArray[1].Name)
     },
+
+
 });
