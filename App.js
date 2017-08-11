343da06dfaf68658eb09370d28598a31a7a2f3ff
Jerry Black <jerry@creativeparanoia.co.uk>
2017-08-11 20:20:27 +0100

    update CSS

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     launch: function() {
-        //Write app code here
+this._myMethod();
+    },
+    _myMethod: function(){
 
-        //API Docs: https://help.rallydev.com/apps/2.1/doc/
-    }
+    },
 });
